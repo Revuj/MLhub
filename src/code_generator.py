bf2878e2d46e654a026cab6e0d5d015d6c3de8e7
Revuj <revuj112@gmail.com>
2021-12-01 18:13:10 +0000

    Executing generated notebook

--- src/code_generator.py
+++ src/code_generator.py
@@ -1,5 +1,7 @@
 import os
+import nbformat
 from jinja2 import Template
+from nbconvert.preprocessors import ExecutePreprocessor
 
 def linear_regression_generator(features_path, labels_path):
   with open(os.path.join("src","model_templates", "linear_regression.ipynb")) as f:
@@ -9,5 +11,17 @@ def linear_regression_generator(features_path, labels_path):
 
   with open(os.path.join("out","parsed_linear_regression.ipynb"), "w") as fh:
     fh.write(parsed_template)
+
+  with open(os.path.join("out","parsed_linear_regression.ipynb")) as f:
+    nb = nbformat.read(f, as_version=4)
+    ep = ExecutePreprocessor(timeout=600, kernel_name='python3')
+    print("Executing python notebook...")
+    ep.preprocess(nb, {'metadata': {'path': 'out/'}})
+    with open(os.path.join('out', 'executed_linear_regression.ipynb'), 'w', encoding='utf-8') as f:
+      nbformat.write(nb, f)
+
+
+
+  
 
 
